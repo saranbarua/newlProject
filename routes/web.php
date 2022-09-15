6cf0e2c9b743e96ce69c3fe10407b5a3405e20e9
Saran barua <Saranbarua2357@gmail.com>
2022-09-15 15:34:40 +0600

    ko

--- routes/web.php
+++ routes/web.php
@@ -25,6 +25,20 @@ Route::get('/blog/1', [PostsController::class, 'show']);
 Route::get('/blog/create', [PostsController::class, 'create']);
 Route::post('/blog', [PostsController::class, 'store']);
 
+
+// PUT & PATCH
+Route::get('/blog/edit/1', [PostsController::class, 'edit']);
+Route::patch('/blog/1', [PostsController::class, 'update']);
+
+// Delete
+Route::delete('/blog/1', [PostsController::class, 'destroy']);
+
+// Resource route for all CRUD operations
+// Route::resource('blog', PostsController::class);
+
+// Route for single action controllers
+Route::get('/', HomeController::class);
+
 Auth::routes();
 
 Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
