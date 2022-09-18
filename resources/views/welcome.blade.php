39d6a6b6ce8fda61816761cdf2605a537a5a9e3a
Saran barua <Saranbarua2357@gmail.com>
2022-09-18 11:55:27 +0600

    niew

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -56,7 +56,7 @@
 
                             <div class="ml-12">
                                 <div class="mt-2 text-gray-600 dark:text-gray-400 text-sm">
-                do u want ?
+                   do u want ?what
                                 </div>
                             </div>
                         </div>
