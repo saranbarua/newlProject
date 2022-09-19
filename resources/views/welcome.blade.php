4454f1982bb2bdb046df71c4489709b53ea30cde
Saran barua <Saranbarua2357@gmail.com>
2022-09-19 22:17:46 +0600

    sa

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -56,7 +56,7 @@
 
                             <div class="ml-12">
                                 <div class="mt-2 text-gray-600 dark:text-gray-400 text-sm">
-                   love u love ? paglu
+                   love u love ? paglu sa 
                                 </div>
                             </div>
                         </div>
