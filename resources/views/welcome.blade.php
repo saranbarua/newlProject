ebb96ca57807942db083958c097e479f0bc12ade
Saran barua <Saranbarua2357@gmail.com>
2022-09-20 19:34:23 +0600

    new

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -17,7 +17,7 @@
         <style>
             body {
 
-                font-family: 'Nunito', sans-serif;
+               font-family: 'Nunito', sans-serif;
             }
         </style>
     </head>
