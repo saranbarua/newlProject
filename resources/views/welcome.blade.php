18e102a614bd5d3da67ccf54ddd27488adbcbf67
Saran barua <Saranbarua2357@gmail.com>
2022-09-20 19:02:15 +0600

    new

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -101,7 +101,7 @@
                         </div>
                     </div>
                 </div>
-     <h1>hel onew new </h1>
+     <h1>hel
                 <div class="flex justify-center mt-4 sm:items-center sm:justify-between">
                     <div class="text-center text-sm text-gray-500 sm:text-left">
                         <div class="flex items-center">
